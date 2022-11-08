e3eb3850321bf3e44df057e6fa8833015e006b81
Mixu <mikohurme@gmail.com>
2022-11-08 18:04:05 +0200

    Office Update
    Mouse model and new computers

--- 3D Horror Game/Assets/Scripts/ComputerCamera.cs
+++ 3D Horror Game/Assets/Scripts/ComputerCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mixutest : MonoBehaviour
+public class ComputerCamera : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
