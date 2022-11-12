5cabcf2f651562e4fedfd44dfb8c459b99c1ba0a
Joelacer <Joelacer@hotmail.com>
2022-11-12 13:03:37 +0200

    Add start to a main menu

---
+++ 3D Horror Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void office()
+    {
+        SceneManager.LoadScene("Office");
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
