d04d8ce169d6c54d7d93cb60e45ae9c2f5c425ab
Joelacer <Joelacer@hotmail.com>
2022-11-15 12:33:47 +0200

    broken system

--- 3D Horror Game/Assets/Scripts/ColorGame.cs
+++ 3D Horror Game/Assets/Scripts/ColorGame.cs
@@ -6,6 +6,7 @@ public class ColorGame : MonoBehaviour
 {
     
     ComputerScreen computerScreen;
+    public GameObject colorgame;
 
     bool game1 = false;
     bool game2 = false;
@@ -15,14 +16,15 @@ public class ColorGame : MonoBehaviour
     public GameObject Showgame2;
     public GameObject Showgame3;
 
-    float blue = 0;
-    float red = 0;
-    float yellow = 0;
-    float green = 0;
+    public float blue = 0;
+    public  float red = 0;
+    public  float yellow = 0;
+    public float green = 0;
 
 
     void Awake()
     {
+
         computerScreen = GameObject.Find("ComputerScreen").GetComponent<ComputerScreen>();
     }
 
@@ -47,6 +49,7 @@ public class ColorGame : MonoBehaviour
             Showgame1.SetActive(true);
             Showgame2.SetActive(false);
             Showgame3.SetActive(false);
+            
 
             if (blue == 1 && red == 1 && yellow == 1 && green == 1)
             {
@@ -55,71 +58,204 @@ public class ColorGame : MonoBehaviour
                 game2 = true;
                 Showgame2.SetActive(true);
                 Showgame1.SetActive(false);
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
             }
         }
 
-        
+        if (game2 == true)
+        {
+            Showgame1.SetActive(false);
+            Showgame2.SetActive(true);
+            Showgame3.SetActive(false);
+
+
+            if (blue == 1 && red == 1 && yellow == 1 && green == 1)
+            {
+                game1 = false;
+                game3 = true;
+                game2 = false;
+                Showgame2.SetActive(false);
+                Showgame3.SetActive(true);
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+
+            }
+        }
+
+        if (game3 == true)
+        {
+            Showgame1.SetActive(false);
+            Showgame2.SetActive(false);
+            Showgame3.SetActive(true);
+
+            if (blue == 1 && red == 1 && yellow == 1 && green == 1)
+            {
+                game1 = false;
+                game3 = false;
+                game2 = false;
+                Showgame2.SetActive(false);
+                Showgame3.SetActive(false);
+                Showgame1.SetActive(false);
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+
+                colorgame.SetActive(false);
+            }
+        }
+
+
     }
 
     public void Blue()
     {
-        if (game1 == true && red==0 && yellow == 0 && green == 0)
+        if (game1 == true)
         {
-            blue += 1;
+            if(red == 0 && yellow == 0 && green == 0)
+            {
+                blue += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
+
         }
-        else
+        
+        if (game2 == true)
         {
-            blue = 0;
-            red = 0;
-            yellow = 0;
-            green = 0;
+            if(green == 1 && red == 0 && yellow == 0)
+            {
+                blue += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
         }
+       
     }
 
     public void Red()
     {
-        if (game1 == true && blue == 1 && yellow == 1 && green == 1)
+        if (game1 == true)
         {
-            red += 1;
+            if(blue == 1 && yellow == 1 && green == 1)
+            {
+                red += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
+
         }
-        else
+        
+        if (game2 == true)
         {
-            blue = 0;
-            red = 0;
-            yellow = 0;
-            green = 0;
+            if(blue == 1 && yellow == 0 && green == 1)
+            {
+                red += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
+
         }
+        
     }
 
     public void Yellow()
     {
-        if(game1 == true && blue == 1 && red == 0 && green == 1)
+        if(game1 == true)
         {
-            yellow += 1;
+            if(blue == 1 && red == 0 && green == 1)
+            {
+                yellow += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
+
         }
-        else
+       
+        if (game2 == true)
         {
-            blue = 0;
-            red = 0;
-            yellow = 0;
-            green = 0;
+            if(blue == 1 && red == 1 && green == 1)
+            {
+                yellow += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
+
         }
+        
     }
 
     public void Green()
     {
-        if(game1 == true && blue == 1 && yellow == 0 && red == 0)
+        if(game1 == true)
         {
-            green += 1;
+            if(blue == 1 && yellow == 0 && red == 0)
+            {
+                green += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
+
 
         }
-        else
+        
+        if (game2 == true)
         {
-            blue = 0;
-            red = 0;
-            yellow = 0;
-            green = 0;
+            if(blue == 0 && yellow == 0 && red == 0)
+            {
+                green += 1;
+            }
+            else
+            {
+                blue = 0;
+                red = 0;
+                yellow = 0;
+                green = 0;
+            }
+
+
         }
+        
     }
 
     // Start is called before the first frame update
