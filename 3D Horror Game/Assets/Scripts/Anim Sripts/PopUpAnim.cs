befd0caae1fc398d2d300ca6e2fbc48d82752677
Joelacer <Joelacer@hotmail.com>
2022-12-31 11:04:48 +0200

    Add pop up thing

---
+++ 3D Horror Game/Assets/Scripts/Anim Sripts/PopUpAnim.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpAnim : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(start());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator start()
+    {
+        yield return new WaitForSeconds(3);
+        animator.SetBool("NewTask", true);
+        yield return new WaitForSeconds(1.20f);
+        animator.SetBool("NewTask", false);
+    }
+}
