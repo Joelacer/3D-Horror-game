4032ee0104932feab12fd90cee6830b71a4029cc
Joelacer <Joelacer@hotmail.com>
2023-02-05 10:54:54 +0200

    worked on the "cant open door" t4ext

--- 3D Horror Game/Assets/Scripts/Cantopendoor.cs
+++ 3D Horror Game/Assets/Scripts/Cantopendoor.cs
@@ -6,23 +6,33 @@ public class Cantopendoor : MonoBehaviour
 {
     public GameObject player;
     public GameObject JanitorDoor;
+    public GameObject BossDoor;
+
+    bool fix = true;
 
     Animator animator;
     DoorAnimation dooranimation;
 
     // Start is called before the first frame update
     void Start()
+
     {
         animator = GetComponent<Animator>();
-        dooranimation = GameObject.Find("door").GetComponent<DoorAnimation>();
+        dooranimation = BossDoor.GetComponent<DoorAnimation>();
+        dooranimation = JanitorDoor.GetComponent<DoorAnimation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(dooranimation.GetifDoor() == false)
+       if(dooranimation.GetifDoor() == false && fix == true && Input.GetKey(KeyCode.E))
         {
             StartCoroutine(start());
+           
+        }
+       else
+        {
+            StopAllCoroutines();
         }
     }
 
@@ -31,8 +41,11 @@ public class Cantopendoor : MonoBehaviour
     IEnumerator start()
     {
         //yield return new WaitForSeconds(17f);
+        fix = false;
         animator.SetBool("NewTask", true);
         yield return new WaitForSeconds(1.20f);
         animator.SetBool("NewTask", false);
+        yield return new WaitForSeconds(2f);
+        fix = true;
     }
 }
