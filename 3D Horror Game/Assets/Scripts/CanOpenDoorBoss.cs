9f3f9db58b853ed1d6dbe2734c0fc33ec77a681b
Joelacer <Joelacer@hotmail.com>
2023-02-25 19:01:29 +0200

    fixxed door not open text

--- 3D Horror Game/Assets/Scripts/CanOpenDoorBoss.cs
+++ 3D Horror Game/Assets/Scripts/CanOpenDoorBoss.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cantopendoor : MonoBehaviour
+public class CanOpenDoorBoss : MonoBehaviour
 {
     public GameObject player;
     public GameObject JanitorDoor;
@@ -23,31 +23,31 @@ public class Cantopendoor : MonoBehaviour
 
     {
         animator = GetComponent<Animator>();
-        //dooranimation = BossDoor.GetComponent<DoorAnimation>();
-        dooranimation = JanitorDoor.GetComponent<DoorAnimation>();
+        dooranimation = BossDoor.GetComponent<DoorAnimation>();
+        //dooranimation = JanitorDoor.GetComponent<DoorAnimation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(dooranimation.GetifDoor() == false && fix == true)
+        if (dooranimation.GetifDoor() == false && fix == true)
         {
             StartCoroutine(start());
-           
+
         }
-       
+
     }
 
-    
+
 
     IEnumerator start()
     {
         //yield return new WaitForSeconds(0.5f);
         //fix = false;
         animator.SetBool("NewTask", true);
-        yield return new WaitForSeconds(1.20f);
+        yield return new WaitForSeconds(1f);
         animator.SetBool("NewTask", false);
-        //yield return new WaitForSeconds(2f);
+        //yield return new WaitForSeconds(0.5f);
         //fix = true;
     }
 }
