60e08eb8d4310371da521189ae3b54255d3f1d45
Joelacer <Joelacer@hotmail.com>
2023-03-04 17:32:55 +0200

    Better start anim AND I Wrote the fixgame correctly

--- 3D Horror Game/Assets/Scripts/Anim Sripts/WalkINAnim.cs
+++ 3D Horror Game/Assets/Scripts/Anim Sripts/WalkINAnim.cs
@@ -8,6 +8,11 @@ public class WalkINAnim : MonoBehaviour
 
     public GameObject Player;
     public GameObject CutSceneCAM;
+    public GameObject BossStart;
+
+    public GameObject FadeIN;
+    public GameObject FadeOut;
+    public GameObject Black;
 
     bool CanSkip;
 
@@ -34,16 +39,38 @@ public class WalkINAnim : MonoBehaviour
 
     IEnumerator WalkIN()
     {
+        FadeIN.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        FadeIN.SetActive(false);
+
         CanSkip = true;
         Player.SetActive(false);
         CutSceneCAM.SetActive(true);
         animator.SetBool("WalkIN", true);
-        yield return new WaitForSeconds(7f);
+        yield return new WaitForSeconds(3f);
         Sound.PlaySound("bossStart");
         yield return new WaitForSeconds(10f);
-        Player.SetActive(true);
-        CutSceneCAM.SetActive(false);
+
+        FadeOut.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        Black.SetActive(true);
+        FadeOut.SetActive(false);
+
+        //Black.SetActive(true);
+        BossStart.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        
+        FadeIN.SetActive(true);
+        Black.SetActive(false);
         animator.SetBool("WalkIN", false);
+        Player.SetActive(true);
+        FadeIN.SetActive(false);
+        CutSceneCAM.SetActive(false);
+        
+
+        //Player.SetActive(true);
+        //CutSceneCAM.SetActive(false);
+        //animator.SetBool("WalkIN", false);
 
     }
 }
